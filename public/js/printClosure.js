d4c8ae34c292ecb8a51bcef3fcbe507fa43a6508
aochinwen <85218536+aochinwen@users.noreply.github.com>
2021-10-24 01:30:07 +0800

    added print to PDF

--- public/js/printClosure.js
+++ public/js/printClosure.js
@@ -1,6 +1,7 @@
 function printClosure(){
+    document.getElementById("divClosureTable").style.display = "block"
     layers = ['pending', 'approved'];
-
+    alert('Click on closure to add to table')
     //populate detail panel
     layers.forEach(layer => {
         map.on('click', layer, (e) => {
@@ -28,4 +29,35 @@ function printClosure(){
             
         });
     });
+}
+
+function createPDF() {
+    let sTable = document.getElementById('divClosureTable').innerHTML;
+    var img = new Image();
+    var mapCanvas = document.querySelector('.mapboxgl-canvas');
+    img.src = mapCanvas.toDataURL();
+    
+
+    var style = "<style>";
+    style = style + "table {width: 100%;font: 17px Calibri;}";
+    style = style + "table, th, td {border: solid 1px #DDD; border-collapse: collapse;";
+    style = style + "padding: 2px 3px;text-align: center;}";
+    style = style + "</style>";
+
+    // CREATE A WINDOW OBJECT.
+    let win = window.open('', '', 'height=700,width=700');
+
+    win.document.write('<html><head>');
+    win.document.write('<title>Closure List</title>');   // <title> FOR PDF HEADER.
+    win.document.write(style);          // ADD STYLE INSIDE THE HEAD TAG.
+    win.document.write('</head>');
+    win.document.write('<body>');
+    win.document.body.appendChild(img);
+    win.document.write('<span> List of Closures </span>')
+    win.document.write(sTable);         // THE TABLE CONTENTS INSIDE THE BODY TAG.
+    win.document.write('</body></html>');
+
+    win.document.close(); 	// CLOSE THE CURRENT WINDOW.
+
+    win.print();    // PRINT THE CONTENTS.
 }
